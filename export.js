ed319c2b0c11a7805784505702fe7f257c60f95e
suresh@mydigitaloffice.ca <77141387+udayakumar-mdo@users.noreply.github.com>
2021-01-27 10:41:01 +0000

    exported dev collections

--- export.js
+++ export.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const parseSchema = require('mongodb-schema');
 
 /*------ db connection properties --------*/
-
-const database = 'local_mdo';
+const database = 'perspective_10_08_2020';
 const user = 'local_test';
 const pass = 'local_test_123#';
 const url = 'localhost:27017';
@@ -13,12 +12,166 @@ const url = 'localhost:27017';
 /*------------------------------------------*/
 
 let directory = "./exports";
-let collections = ['test', 'test1'];
-const limit = 10 //max number of documents per collection, to be exported
+//let collections = ['hmgglcodemasters'];
+let collections = ['users',
+    'webapitokens',
+    'autosequences',
+    'hoteldashboardcalculations',
+    'hotelbudgetdashboardcalculations',
+    'accountingtoolemailogs',
+    'accountingtoolmasters',
+    'accountingtoolmasterproperties',
+    'accountingtoolproperties',
+    'adphotelmasters',
+    'auditlogs',
+    'boardcastmessages',
+    'boardcastmessagelogs',
+    'cashdescriptionmappings',
+    'catagories',
+    'categorydescriptionmappings',
+    'cities',
+    'contactsupport_logs',
+    'countries',
+    'createowntablemappings',
+    'createowntablemappingsgroups',
+    'createowntablemappingsgroupdetails',
+    'createowntablemappingmasters',
+    'customersupportattachments',
+    'dailydatahotelmappings',
+    'dashboardcollapsedhotelgroups',
+    'dashboardsettings',
+    'departments',
+    'dynamicshotelmappings',
+    'emailconfigurations',
+    'emailconfigurationdays',
+    'emailconfigurationotherrecipients',
+    'emailconfigurationreports',
+    'emailconfigurationschedules',
+    'emailescalationconfigurations',
+    'emailescalationnotificationlogs',
+    'emailnotifications',
+    'emailpdfreportcolumnmasters',
+    'emailreportlogs',
+    'emailreportlogs_mirrors',
+    'emailuserhoteltokens',
+    'errorlogs',
+    'events',
+    'expenseformconfigurations',
+    'failedemaillists',
+    'fbpagehotelmappings',
+    'foodnbeveragerevenues',
+    'glcodehotelrevenues',
+    'glcodemasters',
+    'googleplacehotelmappings',
+    'hotelaccountingtoolpropertymappings',
+    'hotelaragings',
+    'hotelavendraalerts',
+    'hotelavendraalertdetails',
+    'hotelavendraalertgroups',
+    'hotelavendraalertinsights',
+    'hotelavendraalertinsightlevels',
+    'hotelavendraalertinsighttypes',
+    'hotelavendraalertnonmfddetaildatas',
+    'hotelavendraalertsubgroups',
+    'hotelbudgets',
+    'hotelbudgetexpenseforms',
+    'hotelcalculationlogtables',
+    'hotelcashdashboardcalculations',
+    'hotelcomments',
+    'hoteleffectivenesscalculations',
+    'hoteleffectivenessmappings',
+    'hotelexternaldatas',
+    'hotelgmloginmappings',
+    'hotelgroups',
+    'hotelgroupxrefs',
+    'hotelgsses',
+    'hotelguestledgers',
+    'hotelqcreports',
+    'hotelqcreportglcodes',
+    'hotelrevenues',
+    'hotelroomstatuses',
+    'hotelroomstatusdashboardcalculations',
+    'hotels',
+    'hotelservices',
+    'hotelsourcetitles',
+    'hotelweatherdatas',
+    'hramastertables',
+    'imagesiloconfigs',
+    'importlogs',
+    'labordatahotelmappings',
+    'm3mastertables',
+    'missingdatesmasters',
+    'organisationcredentials',
+    'otainsightapidataorganisations',
+    'otainsighthotelmappings',
+    'pdfcolumnsortconfigs',
+    'pdfgroupcategories',
+    'pickupreports',
+    'pmsmappings',
+    'pmsmappingmasters',
+    'pmsmasters',
+    'qbdescriptionmappings',
+    'qboaccesstokens',
+    'questionmasters',
+    'quickbookscompanyfilehotelmappings',
+    'quickbooksdesktopexpensedatatobill_logs',
+    'quickbooksdesktopmappings',
+    'revenuemasters',
+    'revenuemaster_logs',
+    'revenuetransactions',
+    'revenuetransaction_logs',
+    'revinateapidataorganisations',
+    'revinatehoteldatas',
+    'revinatehotelmappings',
+    'roleaccesses',
+    'rolebasedpermissions',
+    'roles',
+    'securityhashs',
+    'settingsbygroups',
+    'sorthotelbygroups',
+    'starmonthreports',
+    'starreports',
+    'states',
+    'strrevparweekreports',
+    'stradrmonthreports',
+    'strcompsets',
+    'stroccmonthreports',
+    'strrevparmonthreports',
+    'stroccweekreports',
+    'stradrweekreports',
+    'systempermisions',
+    'tripadvisorhotelmappings',
+    'trustyousurveyreportdatas',
+    'userbasedpermissions',
+    'userhotelgroupxrefs',
+    'userhotelxrefs',
+    'userroleaccesses',
+    'vendors',
+    'yelphotelmappings',
+    'templatemarsters',
+    'templatemastermappings',
+    'personalviews',
+    'kpimarsters',
+    'graphmarsters',
+    'customkpis',
+    'pnlcustomizeraw',
+    'pnlmonthlycolorder',
+    'createpropertydashboardtablemappings',
+    'propertychartconfig',
+    'screenshotschedulers',
+    'operaoutoforderbyreasonreports',
+    'mdoglcodemasters',
+    'hmgglcodemaster',
+    'mdoglcodehmgmappings',
+    'createpnltablemappings',
+    'hotelexpensedashboardcalculations'
+];
+const limit = 20000 //max number of documents per collection, to be exported
 const reverse = false;
 
-const uri = `mongodb://${user}:${pass}@${url}/${database}`;
-
+//const uri = `mongodb://${user}:${pass}@${url}/${database}`;
+//const uri = `mongodb://remote_dev:8Z0gPBVWz2iz8yjh@myp-shard-00-00-zjri4.mongodb.net:27017,myp-shard-00-01-zjri4.mongodb.net:27017,myp-shard-00-02-zjri4.mongodb.net:27017/perspective_10_08_2020?ssl=true&replicaSet=MYP-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1`
+const uri = `mongodb://remote_dev:8Z0gPBVWz2iz8yjh@myp-shard-00-00-zjri4.mongodb.net:27017,myp-shard-00-01-zjri4.mongodb.net:27017,myp-shard-00-02-zjri4.mongodb.net:27017/perspective_10_08_2020?ssl=true&replicaSet=MYP-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1`
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -37,6 +190,7 @@ async function colelctionExport() {
         const db = client.db(database);
 
         for await (collect_name of collections) {
+			
             collection = db.collection(collect_name);
             result = collection.find({});
             if(reverse)
